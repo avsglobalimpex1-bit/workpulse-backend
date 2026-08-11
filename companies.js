c632c736c401ae31ea74ea6ad67baeb060a1b444
avsglobalimpex1-bit <avsglobalimpex.1@gmail.com>
2026-08-11 18:06:03 +0530

    Update companies.js

--- companies.js
+++ companies.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('./db');
 
 // Create a new tenant/company (used during Owner sign-up / bootstrap)
 router.post('/', async (req, res) => {
